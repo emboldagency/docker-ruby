6da4420d4b6dd91ab1bc8d3dacbc5bed4079c26c
Tyler Kocheff <tyler@embold.com>
2023-11-17 15:09:09 -0500

    emBold registry!

--- main.tf
+++ main.tf
@@ -154,7 +154,7 @@ resource "docker_container" "pg" {
 }
 
 resource "docker_image" "ruby302" {
-    name          = "emboldcreative/ruby:2.6.9-ubuntu22.04"
+    name          = "registry.embold.app/ruby:2.6.9-ubuntu22.04"
     build {
         context = "./build"
     }

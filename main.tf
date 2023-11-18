a4398ead0c894c0ea03243750ccbfd3b851a1207
Tyler Kocheff <tyler@embold.com>
2023-11-17 19:08:16 -0500

    Fix image name

--- main.tf
+++ main.tf
@@ -154,7 +154,7 @@ resource "docker_container" "pg" {
 }
 
 resource "docker_image" "ruby302" {
-    name          = "registry.embold.app/ruby:3.0.2-ubuntu22.04"
+    name          = "registry.embold.app/ruby:3.0.2-ubuntu20.04"
     build {
         context = "./build"
     }

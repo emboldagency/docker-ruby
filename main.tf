ea201f16d6fdf68f67a0c0ded47cfd54f8405ec8
Tyler Kocheff <tyler@embold.com>
2023-12-01 03:26:49 -0500

    Remove containers/networks on workspace shutdown, prep for dynamic build

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_providers {
-    coder = {
-      source  = "coder/coder"
-      version = "~> 0.12.1"
+    required_providers {
+        coder = {
+            source  = "coder/coder"
+            version = "~> 0.12.1"
+        }
+        docker = {
+            source  = "kreuzwerker/docker"
+            version = "~> 3.0.2"
+        }
     }
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0.2"
-    }
-  }
 }
 
 provider "coder" {
@@ -23,25 +23,29 @@ data "coder_provisioner" "me" {
 data "coder_workspace" "me" {
 }
 
+locals {
+    devurl = "https://webapp--main--${data.coder_workspace.me.name}--${data.coder_workspace.me.owner}.embold.app"
+}
+
 resource "coder_agent" "main" {
-  arch                    = data.coder_provisioner.me.arch
-  os                      = "linux"
-  startup_script_timeout  = 180
-  startup_script_behavior = "blocking"
-  metadata {
-    display_name = "CPU Usage"
-    key          = "0_cpu_usage"
-    script       = "coder stat cpu"
-    interval     = 10
-    timeout      = 1
-  }
-  metadata {
-    display_name = "RAM Usage"
-    key          = "1_ram_usage"
-    script       = "coder stat mem"
-    interval     = 10
-    timeout      = 1
-  }
+    arch                    = data.coder_provisioner.me.arch
+    os                      = "linux"
+    startup_script_timeout  = 180
+    startup_script_behavior = "blocking"
+    metadata {
+        display_name = "CPU Usage"
+        key          = "0_cpu_usage"
+        script       = "coder stat cpu"
+        interval     = 10
+        timeout      = 1
+    }
+    metadata {
+        display_name = "RAM Usage"
+        key          = "1_ram_usage"
+        script       = "coder stat mem"
+        interval     = 10
+        timeout      = 1
+    }
     metadata {
         display_name = "Disk Usage"
         key          = "2_disk_usage"
@@ -58,48 +62,49 @@ resource "coder_agent" "main" {
         interval     = 10
         timeout      = 1
     }
-  env = {
-    "CODER_USERNAME"       = data.coder_workspace.me.owner
-    "CODER_WORKSPACE_PORT" = 3000
-    "CODER_WORKSPACE_NAME" = data.coder_workspace.me.name
-    "APP"                  = data.coder_workspace.me.name
-    "GIT_AUTHOR_NAME"      = data.coder_workspace.me.owner
-    "GIT_COMMITTER_NAME"   = data.coder_workspace.me.owner
-    "GIT_AUTHOR_EMAIL"     = data.coder_workspace.me.owner_email
-    "GIT_COMMITTER_EMAIL"  = data.coder_workspace.me.owner_email
-    "PGHOST"               = "postgres"
-  }
+    env = {
+        "APP"                  = data.coder_workspace.me.name
+        "CODER_USERNAME"       = data.coder_workspace.me.owner
+        "CODER_WORKSPACE_NAME" = data.coder_workspace.me.name
+        "CODER_WORKSPACE_PORT" = 3000
+        "DEVURL"               = "${local.devurl}"
+        "GIT_AUTHOR_EMAIL"     = data.coder_workspace.me.owner_email
+        "GIT_AUTHOR_NAME"      = data.coder_workspace.me.owner
+        "GIT_COMMITTER_EMAIL"  = data.coder_workspace.me.owner_email
+        "GIT_COMMITTER_NAME"   = data.coder_workspace.me.owner
+        "PGHOST"               = "postgres"
+    }
     startup_script = <<-EOT
         set -e
-        /bin/bash /coder/configure
+        /bin/bash /coder/scripts/configure
     EOT
 }
 
 resource "docker_volume" "home_volume" {
-  name = "coder-${lower(data.coder_workspace.me.owner)}-${lower(data.coder_workspace.me.name)}-${data.coder_workspace.me.id}-home"
-  # Protect the volume from being deleted due to changes in attributes.
-  lifecycle {
-    ignore_changes = all
-  }
-  # Add labels in Docker to keep track of orphan resources.
-  labels {
-    label = "coder.owner"
-    value = data.coder_workspace.me.owner
-  }
-  labels {
-    label = "coder.owner_id"
-    value = data.coder_workspace.me.owner_id
-  }
-  labels {
-    label = "coder.workspace_id"
-    value = data.coder_workspace.me.id
-  }
-  # This field becomes outdated if the workspace is renamed but can
-  # be useful for debugging or cleaning out dangling volumes.
-  labels {
-    label = "coder.workspace_name_at_creation"
-    value = data.coder_workspace.me.name
-  }
+    name = "coder-${lower(data.coder_workspace.me.owner)}-${lower(data.coder_workspace.me.name)}-${data.coder_workspace.me.id}-home"
+    # Protect the volume from being deleted due to changes in attributes.
+    lifecycle {
+        ignore_changes = all
+    }
+    # Add labels in Docker to keep track of orphan resources.
+    labels {
+        label = "coder.owner"
+        value = data.coder_workspace.me.owner
+    }
+    labels {
+        label = "coder.owner_id"
+        value = data.coder_workspace.me.owner_id
+    }
+    labels {
+        label = "coder.workspace_id"
+        value = data.coder_workspace.me.id
+    }
+    # This field becomes outdated if the workspace is renamed but can
+    # be useful for debugging or cleaning out dangling volumes.
+    labels {
+        label = "coder.workspace_name_at_creation"
+        value = data.coder_workspace.me.name
+    }
 }
 
 resource "docker_volume" "postgres_volume" {
@@ -129,20 +134,20 @@ resource "docker_volume" "postgres_volume" {
     }
 }
 
-resource "docker_network" "workspace_network" {
-  name   = "coder-${lower(data.coder_workspace.me.owner)}-${lower(data.coder_workspace.me.name)}-network"
-  driver = "bridge"
+resource "docker_network" "workspace" {
+    name  = "coder-${lower(data.coder_workspace.me.owner)}-${lower(data.coder_workspace.me.name)}-network"
+    count = data.coder_workspace.me.start_count
 }
 
 resource "docker_container" "pg" {
+    count        = data.coder_workspace.me.start_count
     name         = "coder-${lower(data.coder_workspace.me.owner)}-${lower(data.coder_workspace.me.name)}-pg"
     image        = "postgres:15"
-    restart      = "unless-stopped"
     hostname     = "postgres"
-    network_mode = docker_network.workspace_network.name
+    network_mode = docker_network.workspace[count.index].name
     env = [
         "POSTGRES_ROOT_PASSWORD=embold",
-        "POSTGRES_DB=${data.coder_workspace.me.name}",
+        "POSTGRES_DB=${replace(data.coder_workspace.me.name, "-", "_")}",
         "POSTGRES_USER=embold",
         "POSTGRES_PASSWORD=embold",
     ]
@@ -153,17 +158,17 @@ resource "docker_container" "pg" {
     }
 }
 
-resource "docker_image" "ruby302" {
-    name          = "registry.embold.app/ruby:3.0.2-ubuntu20.04"
+resource "docker_image" "ruby" {
+    name         = "registry.embold.app/ruby:3.0.2-ubuntu20.04"
+    keep_locally = true
     build {
         context = "./build"
     }
-    keep_locally = true
 }
 
 resource "docker_container" "workspace" {
     count = data.coder_workspace.me.start_count
-    image = docker_image.ruby302.name
+    image = docker_image.ruby.name
     # Uses lower() to avoid Docker restriction on container names.
     name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
     # Hostname makes the shell more user friendly: coder@my-workspace:~$
@@ -180,7 +185,7 @@ resource "docker_container" "workspace" {
         volume_name    = docker_volume.home_volume.name
         read_only      = false
     }
-    network_mode = docker_network.workspace_network.name
+    network_mode = docker_network.workspace[count.index].name
     # Add labels in Docker to keep track of orphan resources.
     labels {
         label = "coder.owner"
@@ -195,23 +200,19 @@ resource "docker_container" "workspace" {
         value = data.coder_workspace.me.id
     }
     labels {
-    label = "coder.workspace_name"
-    value = data.coder_workspace.me.name
+        label = "coder.workspace_name"
+        value = data.coder_workspace.me.name
     }
 }
 
-resource "coder_app" "ruby_app" {
+resource "coder_app" "web_app" {
     agent_id  = coder_agent.main.id
+    display_name = "Web App"
     slug      = "webapp"
-    icon      = "/icon/ruby.png"
+    icon      = "/emojis/1f310.png"
     url       = "http://localhost:3000"
     subdomain = true
     share     = "public"
-    healthcheck {
-        url       = "http://localhost:3000/up"
-        interval  = 10
-        threshold = 30
-    }
 }
 
 resource "coder_metadata" "container_info" {
@@ -220,11 +221,11 @@ resource "coder_metadata" "container_info" {
 
     item {
         key   = "image"
-        value = docker_image.ruby302.name
+        value = docker_image.ruby.name
     }
     item {
         key   = "devurl"
-        value = "https://webapp--main--${data.coder_workspace.me.name}--${data.coder_workspace.me.owner}.embold.app/"
+        value = "${local.devurl}"
     }
 }
 

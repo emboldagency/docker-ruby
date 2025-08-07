63fdd8274a7ccee0644bfd5f805dadc75a36a9ea
tyler <84726170+embold-tyler@users.noreply.github.com>
2025-08-06 19:04:45 -0700

    :sparkles: Add pulsar magic template

--- main.tf
+++ main.tf
@@ -31,7 +31,6 @@ data "coder_workspace" "me" {}
 
 data "coder_workspace_owner" "me" {}
 
-
 locals {
   app                   = lower(try(length(local.pulsar_app_name), 0) > 0 ? local.pulsar_app_name : local.workspace_name)
   db_name               = replace(local.app, "-", "_")
@@ -40,6 +39,7 @@ locals {
   github_token          = data.coder_external_auth.github.access_token
   postgres_version      = data.coder_parameter.postgres_version.value
   pulsar_app_name       = data.coder_parameter.pulsar_app_name.value
+  pulsar_magic_template = data.coder_parameter.pulsar_magic_template.value
   resource_name_prefix  = "coder-${local.user_username}-${local.workspace_name}"
   template_version      = "v1.0.0"
   rails_master_key      = trimspace(data.coder_parameter.rails_master_key.value) != "" ? "RAILS_MASTER_KEY=${trimspace(data.coder_parameter.rails_master_key.value)}" : ""
@@ -69,6 +69,15 @@ data "coder_parameter" "pulsar_app_name" {
   icon        = "/icon/coder.svg"
   type        = "string"
   default     = ""
+  mutable     = true
+}
+
+data "coder_parameter" "pulsar_magic_template" {
+  name        = "Pulsar Magic Template?"
+  description = "Should we use the Pulsar magic template to dynamically build the Pulsar configuration?"
+  type        = "bool"
+  icon        = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 576 512'%3E%3C!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--%3E%3Cpath fill='%23009dff' d='M234.7 42.7L197 56.8c-3 1.1-5 4-5 7.2s2 6.1 5 7.2l37.7 14.1L248.8 123c1.1 3 4 5 7.2 5s6.1-2 7.2-5l14.1-37.7L315 71.2c3-1.1 5-4 5-7.2s-2-6.1-5-7.2L277.3 42.7 263.2 5c-1.1-3-4-5-7.2-5s-6.1 2-7.2 5L234.7 42.7zM46.1 395.4c-18.7 18.7-18.7 49.1 0 67.9l34.6 34.6c18.7 18.7 49.1 18.7 67.9 0L529.9 116.5c18.7-18.7 18.7-49.1 0-67.9L495.3 14.1c-18.7-18.7-49.1-18.7-67.9 0L46.1 395.4zM484.6 82.6l-105 105-23.3-23.3 105-105 23.3 23.3zM7.5 117.2C3 118.9 0 123.2 0 128s3 9.1 7.5 10.8L64 160l21.2 56.5c1.7 4.5 6 7.5 10.8 7.5s9.1-3 10.8-7.5L128 160l56.5-21.2c4.5-1.7 7.5-6 7.5-10.8s-3-9.1-7.5-10.8L128 96 106.8 39.5C105.1 35 100.8 32 96 32s-9.1 3-10.8 7.5L64 96 7.5 117.2zm352 256c-4.5 1.7-7.5 6-7.5 10.8s3 9.1 7.5 10.8L416 416l21.2 56.5c1.7 4.5 6 7.5 10.8 7.5s9.1-3 10.8-7.5L480 416l56.5-21.2c4.5-1.7 7.5-6 7.5-10.8s-3-9.1-7.5-10.8L480 352l-21.2-56.5c-1.7-4.5-6-7.5-10.8-7.5s-9.1 3-10.8 7.5L416 352l-56.5 21.2z'/%3E%3C/svg%3E" # font-awesome magic wand. alt: "/emojis/1fa84.png"
+  default     = false
   mutable     = true
 }
 
@@ -131,6 +140,7 @@ resource "coder_agent" "main" {
     GIT_AUTHOR_EMAIL      = local.user_email
     GIT_COMMITTER_NAME    = local.user_full_name
     GIT_COMMITTER_EMAIL   = local.user_email
+    PULSAR_MAGIC_TEMPLATE = local.pulsar_magic_template
   }
   metadata {
     display_name = "CPU Usage"
